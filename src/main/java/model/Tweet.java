4c4acd5136bc9fa612d187bc0f4522511a016950
Leonardo Santos <leonrdsnt@gmail.com>
2020-08-04 15:55:41 -0300

    Adiciona equals e hashCode ao Tweet

--- src/main/java/model/Tweet.java
+++ src/main/java/model/Tweet.java
@@ -33,7 +33,44 @@ public class Tweet {
 	public void setUsuario(Usuario usuario) {
 		this.usuario = usuario;
 	}
-	
-	
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((conteudo == null) ? 0 : conteudo.hashCode());
+		result = prime * result + ((dataCriacao == null) ? 0 : dataCriacao.hashCode());
+		result = prime * result + id;
+		result = prime * result + ((usuario == null) ? 0 : usuario.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Tweet other = (Tweet) obj;
+		if (conteudo == null) {
+			if (other.conteudo != null)
+				return false;
+		} else if (!conteudo.equals(other.conteudo))
+			return false;
+		if (dataCriacao == null) {
+			if (other.dataCriacao != null)
+				return false;
+		} else if (!dataCriacao.equals(other.dataCriacao))
+			return false;
+		if (id != other.id)
+			return false;
+		if (usuario == null) {
+			if (other.usuario != null)
+				return false;
+		} else if (!usuario.equals(other.usuario))
+			return false;
+		return true;
+	}
 
 }
