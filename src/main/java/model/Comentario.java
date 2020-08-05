5721ade336c48836d2ab4acd1fdf589f4c27746b
Leonardo Santos <leonrdsnt@gmail.com>
2020-08-05 13:03:36 -0300

    Adiciona equals e hashCode ao Comentario

--- src/main/java/model/Comentario.java
+++ src/main/java/model/Comentario.java
@@ -40,6 +40,50 @@ public class Comentario {
 	public void setTweet(Tweet tweet) {
 		this.tweet = tweet;
 	}
-	
-	
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((conteudo == null) ? 0 : conteudo.hashCode());
+		result = prime * result + ((dataCriacao == null) ? 0 : dataCriacao.hashCode());
+		result = prime * result + id;
+		result = prime * result + ((tweet == null) ? 0 : tweet.hashCode());
+		result = prime * result + ((usuario == null) ? 0 : usuario.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Comentario other = (Comentario) obj;
+		if (conteudo == null) {
+			if (other.conteudo != null)
+				return false;
+		} else if (!conteudo.equals(other.conteudo))
+			return false;
+		if (dataCriacao == null) {
+			if (other.dataCriacao != null)
+				return false;
+		} else if (!dataCriacao.equals(other.dataCriacao))
+			return false;
+		if (id != other.id)
+			return false;
+		if (tweet == null) {
+			if (other.tweet != null)
+				return false;
+		} else if (!tweet.equals(other.tweet))
+			return false;
+		if (usuario == null) {
+			if (other.usuario != null)
+				return false;
+		} else if (!usuario.equals(other.usuario))
+			return false;
+		return true;
+	}
+
 }
