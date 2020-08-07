48473504cc05a02b35bd3d679e465b4767a3184b
Leonardo Santos <leonrdsnt@gmail.com>
2020-08-07 14:59:34 -0300

    Implementa os métodos pesquisar e contar do UsuarioRepository

--- src/main/java/repository/UsuarioRepository.java
+++ src/main/java/repository/UsuarioRepository.java
@@ -9,6 +9,8 @@ import java.util.List;
 
 import javax.ejb.Stateless;
 
+import org.apache.commons.lang3.StringUtils;
+
 import model.Usuario;
 import model.exceptions.ErroAoConectarNaBaseException;
 import model.exceptions.ErroAoConsultarBaseException;
@@ -16,7 +18,6 @@ import model.seletor.UsuarioSeletor;
 
 @Stateless
 public class UsuarioRepository extends AbstractCrudRepository {
-
 
 	public void inserir(Usuario usuario) throws ErroAoConectarNaBaseException, ErroAoConsultarBaseException {
 		//abrir uma conexao com o banco
@@ -86,17 +87,22 @@ public class UsuarioRepository extends AbstractCrudRepository {
 	}
 
 	public List<Usuario> listarTodos() throws ErroAoConectarNaBaseException, ErroAoConsultarBaseException {
+		return pesquisar(new UsuarioSeletor());
+	}
+
+	public List<Usuario> pesquisar(UsuarioSeletor seletor) throws ErroAoConsultarBaseException, ErroAoConectarNaBaseException {
 		List<Usuario> resultado = new ArrayList<>();
 
 		try (Connection c = this.abrirConexao()) {
-			PreparedStatement ps = c.prepareStatement("select id, nome from usuario");
+			StringBuilder sql = new StringBuilder("SELECT id, nome FROM usuario");
+			adicionarFiltros(sql, seletor);
+
+			PreparedStatement ps = c.prepareStatement(sql.toString());
+			adicionarParametros(ps, seletor);
 
 			ResultSet rs = ps.executeQuery();
 			while (rs.next()) {
-				Usuario user = new Usuario();
-				user.setId(rs.getInt("id"));
-				user.setNome(rs.getString("nome"));
-				resultado.add(user);
+				resultado.add(criarModel(rs));
 			}
 			rs.close();
 			ps.close();
@@ -107,12 +113,64 @@ public class UsuarioRepository extends AbstractCrudRepository {
 		return resultado;
 	}
 
-	public List<Usuario> pesquisar(UsuarioSeletor seletor) throws ErroAoConsultarBaseException, ErroAoConectarNaBaseException {
-		return null;
+	public Long contar(UsuarioSeletor seletor) throws ErroAoConsultarBaseException, ErroAoConectarNaBaseException {
+		try (Connection c = this.abrirConexao()) {
+			StringBuilder sql = new StringBuilder("SELECT count(*) as total FROM usuario");
+			adicionarFiltros(sql, seletor);
+
+			PreparedStatement ps = c.prepareStatement(sql.toString());
+			adicionarParametros(ps, seletor);
+
+			long total = 0;
+			ResultSet rs = ps.executeQuery();
+			if (rs.next()) {
+				total = rs.getLong("total");
+			}
+			rs.close();
+			ps.close();
+
+			return total;
+		} catch (SQLException e) {
+			throw new ErroAoConsultarBaseException("Ocorreu um erro ao listar todos os usuários", e);
+		}
 	}
 
-	public Long contar(UsuarioSeletor seletor) throws ErroAoConsultarBaseException, ErroAoConectarNaBaseException {
-		return null;
+	private void adicionarFiltros(StringBuilder sql, UsuarioSeletor seletor) {
+		if (!seletor.possuiFiltro()) return;
+		sql.append(" WHERE ");
+
+		boolean primeiroFiltro = true;
+		if (seletor.getId() != null) {
+			primeiroFiltro = false;
+			sql.append("id = ? ");
+		}
+		if (!StringUtils.isBlank(seletor.getNome())) {
+			if (!primeiroFiltro) {
+				sql.append("AND ");
+			}
+			sql.append("nome LIKE ?");
+		}
+	}
+
+	private void adicionarParametros(PreparedStatement ps, UsuarioSeletor seletor) throws SQLException {
+		if (!seletor.possuiFiltro()) return;
+
+		int index = 1;
+
+		if (seletor.getId() != null) {
+			ps.setInt(index++, seletor.getId());
+		}
+
+		if (!StringUtils.isBlank(seletor.getNome())) {
+			ps.setString(index++, String.format("%%%s%%", seletor.getNome()));
+		}
+	}
+
+	private Usuario criarModel(ResultSet rs) throws SQLException {
+		Usuario user = new Usuario();
+		user.setId(rs.getInt("id"));
+		user.setNome(rs.getString("nome"));
+		return user;
 	}
 
 }
